5320a5c49bf69eab2d2be9c1cb6103fd6960a1f0
Aparna Dutta <aparnad198@gmail.com>
2021-12-06 19:54:40 -0500

    added evaluate

--- evaluate_test.py
+++ evaluate_test.py
@@ -1,24 +1,14 @@
 import numpy as np
-# from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-from sklearn.metrics import classification_report
 
 all_preds = np.load('./prediction_probabilities.npy')
 
-preds = [pair[0] for pair in all_preds if not np.array_equal(pair, np.array([-1, -1]))]
-true_labels = [pair[1] for pair in all_preds if not np.array_equal(pair, np.array([-1, -1]))]
+conf_mat = np.zeros((6, 6))
 
-print(len(all_preds))
+for i in range(len(all_preds)):
+    pad_row = np.array([-1, -1])
+    for word in all_preds[i]:
+        if not np.array_equal(word, pad_row):
+            conf_mat[word[0][word[1]]] += 1
 
-# num_correct, total = 0, 0
-
-# for i in range(len(all_preds)):
-#     pad_row = np.array([-1, -1])
-#     for word in all_preds[i]:
-#         if not np.array_equal(word, pad_row):
-#             total += 1
-#             if word[0] == word[1]:
-#                 num_correct += 1
-#
-# accuracy = num_correct / total
-# print("accuracy:", accuracy)
+print("confusion matrix:", conf_mat)
 
